28602475ffb5fa3158675ea15c5d0a0bc7ac03b3
MANOJ_SM <manumedleri@gmail.com>
2025-02-16 18:46:06 +0530

    updated file

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+
 app = Flask(__name__)
+
+# Attach Prometheus metrics
+metrics = PrometheusMetrics(app)
 
 @app.route('/')
 def home():
-    return "Hello, DevOps!"
+    return "Hello, Flask!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
